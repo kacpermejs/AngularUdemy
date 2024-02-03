5646abe9c19943463d3d1d82ad29d674b744bca2
Kacper Mejsner <kacpermejs@gmail.com>
2024-02-03 14:56:27 +0100

    shopping list prototype

--- 1_RecipeBook/src/app/shopping-list/shopping-list.component.ts
+++ 1_RecipeBook/src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { Ingredient } from '../models/ingredient.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [ShoppingListEditComponent],
+  imports: [CommonModule, ShoppingListEditComponent],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomato', 2),
+    new Ingredient('Potato', 8),
+  ];
 
 }
