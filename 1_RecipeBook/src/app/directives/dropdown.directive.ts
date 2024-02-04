ee15b519b44b8caf66fda8fed64b1a83ffbcb20f
Kacper Mejsner <kacpermejs@gmail.com>
2024-02-04 18:32:27 +0100

    better dropdown directive

--- 1_RecipeBook/src/app/directives/dropdown.directive.ts
+++ 1_RecipeBook/src/app/directives/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
@@ -7,10 +7,10 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen: boolean = false;
 
-  constructor() { }
+  constructor(private elRef: ElementRef) {}
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
 
 }
