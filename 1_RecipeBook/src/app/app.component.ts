7cbd14d126f8c2a291d5cb5f65c092fad0426e23
Kacper Mejsner <kacpermejs@gmail.com>
2024-02-06 12:21:14 +0100

    basic routing

--- 1_RecipeBook/src/app/app.component.ts
+++ 1_RecipeBook/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent, Site } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, ShoppingListComponent, RecipesComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ShoppingListComponent, RecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 1_RecipeBook/src/app/header/header.component.ts
+++ 1_RecipeBook/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { DropdownDirective } from '../directives/dropdown.directive';
+import { RouterModule } from '@angular/router';
 
 export enum Site {
   Shopping = 'shopping',
@@ -10,17 +11,11 @@ export enum Site {
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, DropdownDirective],
+  imports: [CommonModule, DropdownDirective, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   Site = Site;
   collapsed = true;
-
-  @Output() onNavigation = new EventEmitter<Site>;
-
-  navigate(site: Site) {
-    this.onNavigation.emit(site);
-  }
 }
