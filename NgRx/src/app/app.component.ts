746be6ceffcbefe5c0647bd1ce48175bf1ed6860
Kacper Mejsner <kacpermejs@gmail.com>
2024-02-27 17:10:15 +0100

    NgRx initial

---
+++ NgRx/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CounterOutputComponent, CounterControlsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'NgRx';
+}
---
+++ NgRx/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { counterReducer } from './store/counter.reducer';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideStore({
+    counter: counterReducer
+  })]
+};
