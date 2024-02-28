92933541888d0eee2a51b0647cfd4e9bf179321d
Kacper Mejsner <kacpermejs@gmail.com>
2024-02-28 21:21:53 +0100

    Redux DevTools

--- 1_RecipeBook/src/app/app.component.ts
+++ 1_RecipeBook/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, enableProdMode, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent, Site } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth/auth.service';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,13 @@ import { AuthService } from './services/auth/auth.service';
 export class AppComponent implements OnInit{
   title = 'RecipeBook';
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService) {
+    console.log('Env prod: ' + environment.production);
+    if (environment.production) {
+      enableProdMode();
+    }
+    console.log('Is dev mode: ' + isDevMode());
+  }
 
   ngOnInit(): void {
     this.auth.autoLogin();
--- 1_RecipeBook/src/app/app.config.ts
+++ 1_RecipeBook/src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, enableProdMode } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
-
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptorService } from './services/auth/auth-interceptor.service';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
 import * as fromShoppingList from './store/shopping-list/shopping-list.reducer';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +24,7 @@ export const appConfig: ApplicationConfig = {
     }),
     provideEffects([
       
-    ])
+    ]),
+    provideStoreDevtools({logOnly: environment.production})
 ]
 };
